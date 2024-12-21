c898f2386272474b1aaad21dd3e901debf8e9361
kawsarkabir <devkawsarkabir@gmail.com>
2024-12-21 14:29:42 +0600

    ✨ feat: add header, footer, hero, and task components with asset management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import TaskBoard from "./components/tasks/task-board";
+
 export default function App() {
   return (
     <>
-      <h1>This is from App component</h1>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <Hero />
+        <TaskBoard />
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ src/assets/assets.ts
@@ -0,0 +1,7 @@
+import frame from "../assets/images/frame.png";
+import lws_logo from "../assets/images/lws-logo-en.svg";
+
+export const asset = {
+  lws_logo,
+  frame,
+};
