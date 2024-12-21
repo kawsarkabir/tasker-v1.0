1a9956f69f1d6f862b27b5ee4a16807ecd28e7db
kawsarkabir <devkawsarkabir@gmail.com>
2024-12-21 18:38:49 +0600

    ✨ feat: add default task state and integrate with TaskList component

--- src/components/tasks/task-board.tsx
+++ src/components/tasks/task-board.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import SearchTask from "./search-task";
 import TaskAction from "./task-action";
 import TaskList from "./task-list";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Complete Assingment 02",
+    description: "Complete the assignment 02 before the deadline",
+    tag: ["lws", "react", "assignment"],
+    priority: "high",
+    isFavorite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -12,7 +22,7 @@ export default function TaskBoard() {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
